84d1fc2095ad3d5fe7a3a4eb8cc0736d029049ba
Miguel Galán Lerate <miggaller@alum.us.es>
2025-04-02 11:20:26 +0200

    Feat: Init repository

---
+++ src/main/java/cbd/gr17/secmongo_cli/SecmongoCliApplication.java
@@ -0,0 +1,13 @@
+package cbd.gr17.secmongo_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecmongoCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecmongoCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cbd/gr17/secmongo_cli/SecmongoCliApplicationTests.java
@@ -0,0 +1,13 @@
+package cbd.gr17.secmongo_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecmongoCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
